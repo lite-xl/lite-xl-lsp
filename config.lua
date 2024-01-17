bd9375e21f4c0665c0dd943c963702a8a772dbea
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-01-17 12:56:31 +0100

    Added perl support.

--- config.lua
+++ config.lua
@@ -497,6 +497,23 @@ lspconfig.pylsp = add_lsp {
   verbose = false
 }
 
+---# PerlNavigator - Perl
+--- __Status__: Untested
+--- __Site__: https://github.com/bscan/PerlNavigator
+--- __Installation__: `paru -S perlnavigator`
+lsp.add_server {
+  name = "perlnavigator",
+  language = "Perl",
+  file_patterns = { "%.pl$", "%.pm$" },
+  command = { "perlnavigator" },
+  settings = {
+    perlnavigator = {
+      -- The following setting is only needed if you want to set a custom perl path. It already defaults to "perl"
+      perlPath = "perl"
+    }
+  }
+}
+
 ---# Ruby LSP
 --- __Status__: Untested
 --- __Site__: https://github.com/Shopify/ruby-lsp
